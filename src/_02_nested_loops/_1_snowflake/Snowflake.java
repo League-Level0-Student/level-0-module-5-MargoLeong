5a2b4f7a0818ff0578f2ad25036344c9d586e3e6
MargoLeong <52899083+MargoLeong@users.noreply.github.com>
2020-11-13 17:32:13 -0800

    snow

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -4,104 +4,60 @@ import java.awt.Color;
 
 import org.jointheleague.graphical.robot.Robot;
 
-
 public class Snowflake {
-	
+
 	public static void main(String[] args) throws InterruptedException {
 
 		// #1. Make a new Robot
-Robot bob= new Robot();
+		Robot bob = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-bob.setX(300);
-bob.setY(300);
-
+		bob.setX(300);
+		bob.setY(300);
 
 		// #3. Put the robot's pen down
-bob.penDown();
+		bob.penDown();
 
 		// #4. Set the robot’s speed to 5
-bob.setSpeed(5);
+		bob.setSpeed(5);
 
-		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-for(int i=0; i<6; i++) {
-	
+		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6
+		// times (use i as the counter)
+		for (int i = 0; i < 100; i++) {
 
+			
+			bob.microMove(20);
+			bob.setPenColor(24, 170, 170);
+			bob.microTurn(10);
+			bob.microMove(10);
+			bob.setPenColor(0, 0, 0);
+			bob.microTurn(20);
 
-bob.setPenColor(0,0,0);
-bob.microMove(50);
-bob.turn(60);
-bob.setPenColor(35,172,176);
-bob.microMove(50);
-bob.turn(60);
-bob.setPenColor(0,0,0);
-bob.microMove(50);
-bob.turn(60);
-bob.setPenColor(35,172,176);
-bob.microMove(50);
-bob.turn(60);
-bob.setPenColor(0,0,0);
-bob.microMove(50);
-bob.turn(60);
-bob.setPenColor(35,172,176);
-bob.microMove(50);
-bob.turn(60);
-bob.setPenColor(0,0,0);
-bob.microTurn(60);	
-bob.microMove(50);
-bob.microTurn(60);	
-bob.microMove(50);
-bob.microTurn(60);	
-bob.microMove(50);
-bob.microTurn(60);	
-bob.microMove(50);
-bob.microTurn(60);	
-bob.microMove(50);
-bob.microTurn(60);	
-bob.microMove(50);
-}
-	
+		}
+
 	}
-
-
-
-	
-	
 
 	private static int bob() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-			
-			// #6. Set the pen color to black
 
-			
-			// #7. move the robot by 50
+	// #6. Set the pen color to black
 
-			
-			// #8. turn the robot to the right by 60
-			
-			
-			// #12. set the pen color to blue for even numbers of i
+	// #7. move the robot by 50
 
-			
-			// #13. set the pen color to red for all odd numbers of i
-			
-			
-			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
-		
-					
-					// #11. move the robot by 50 (play the start button)
-					
-				
-			
-			// #14. set the pen color to black
-				
-		
+	// #8. turn the robot to the right by 60
 
+	// #12. set the pen color to blue for even numbers of i
 
+	// #13. set the pen color to red for all odd numbers of i
+
+	// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
+
+	// #10. turn the robot to the left by 60
+
+	// #11. move the robot by 50 (play the start button)
+
+	// #14. set the pen color to black
 
 }
--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -10,6 +10,9 @@ import javax.swing.JApplet;
 public class Code4Life {
 
 	// 1. Ask the user how many hours they spent coding this week.
+JOptionPane.showMessageDialog(null, "how many hours have you coded this week"){
+	
+}
 
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
